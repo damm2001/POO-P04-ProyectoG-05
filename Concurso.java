0229a8eb32690839f4a0bc3cc9faeaaefcf044c0
Meiyin Chang <91926069+meiyincr3@users.noreply.github.com>
2021-11-29 14:12:56 -0500

    Update Concurso.java

--- Concurso.java
+++ Concurso.java
@@ -44,32 +44,5 @@ public class Concurso {
     this.dirigido=dirigido;
     }
 
-    /*public static void agregarConcurso(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Opciones: \n1.-Crear Concurso \n2.-Salir");
-        System.out.print("\nOpción del usuario: ");
-        int opcion_user = sc.nextInt();
-        sc.nextLine();
-        while(opcion_user!=2){
-            System.out.print("\nIngrese el nombre del Concurso: ");
-            String n=sc.nextLine();
-            System.out.print("Ingrese la dirección: ");
-            String d=sc.nextLine();
-            System.out.print("Ingrese el teléfono: ");
-            String t=sc.nextLine();
-            System.out.print("Ingrese el correo electrónico: ");
-            String correo=sc.nextLine();
-            System.out.print("Ingrese la cédula de identidad: ");
-            String c_i=sc.nextLine();
-            System.out.print("Ingrese  el apellido del Dueño de la mascota: ");
-            String last_n=sc.nextLine();
-            owners.add(new DuenoMascota(n,d,t,correo,c_i,last_n));
-            System.out.println("Dueño de Mascota creado.");
-            System.out.println("\n1.-Crear Dueño de Mascota\n2.-Salir");
-            System.out.print("\nOpción del usuario: ");
-            opcion_user = sc.nextInt();
-            sc.nextLine();
-        }sc.close();
-    }   */
 
 }
