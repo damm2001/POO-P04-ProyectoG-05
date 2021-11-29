5b4dc7e1a42cc31ed49c90840371ac0c081e2304
damm2001 <dianmart@espol.edu.ec>
2021-11-28 20:47:27 -0500

    Elimine la clase premio

--- prmio.java
+++
@@ -1,9 +0,0 @@
-public class Premio {
-    public String descripcion;
-    public boolean tieneAuspiciante;
-
-    public Premio(String descripcion,boolean tieneAuspiciante){
-        this.descripcion=descripcion;
-        this.tieneAuspiciante=tieneAuspiciante;
-    }
-}
