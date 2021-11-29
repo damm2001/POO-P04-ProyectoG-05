44bb55831cdad2fc012d588aa561392b6fddbb18
damm2001 <dianmart@espol.edu.ec>
2021-11-28 20:45:32 -0500

    Agregue premio

---
+++ prmio.java
@@ -0,0 +1,9 @@
+public class Premio {
+    public String descripcion;
+    public boolean tieneAuspiciante;
+
+    public Premio(String descripcion,boolean tieneAuspiciante){
+        this.descripcion=descripcion;
+        this.tieneAuspiciante=tieneAuspiciante;
+    }
+}
